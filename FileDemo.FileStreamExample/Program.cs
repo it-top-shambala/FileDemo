b6dc9b73e673ea33373b93b8082a6536fb349d84
Starinin Andrey <starininandrey@gmail.com>
2022-06-13 19:36:51 +0300

    Init

---
+++ FileDemo.FileStreamExample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+namespace FileDemo.FileStreamExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "example.txt";
+            FileStream file = new FileStream(path, FileMode.Create, FileAccess.Write);
+            string text = "Пример text'а";
+            byte[] bytes = Encoding.UTF8.GetBytes(text);
+            file.Write(bytes, 0, bytes.Length);
+            file.Close();
+        }
+    }
+}
---
+++ FileDemo.StreamReaderAndWriterExample/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace FileDemo.StreamReaderAndWriterExample
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string path = "example.txt";
+            /*
+            StreamWriter file = null;
+            try
+            {
+                file = new StreamWriter(path, append: false);
+                file.WriteLine("Пример TEXT'а");
+            }
+            finally
+            {
+                file?.Close();
+            }
+            */
+
+            using (StreamWriter file = new StreamWriter(path, append: false))
+            {
+                file.WriteLine("Пример TEXT'а");
+            }
+            
+            using (StreamReader file = new StreamReader(path))
+            {
+                string str = file.ReadLine();
+                Console.WriteLine(str);
+            }
+        }
+    }
+}
